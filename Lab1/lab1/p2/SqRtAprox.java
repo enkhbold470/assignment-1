536599799069462234c63bf48f676af95720d0f8
Enkhbold Ganbold <enkhbold470@gmail.com>
2024-05-22 16:26:20 -0700

    added some changes

--- Lab1/lab1/p2/SqRtAprox.java
+++ Lab1/lab1/p2/SqRtAprox.java
@@ -6,7 +6,6 @@ Due Date: April.24
 Date Submitted: April.22
  */
 import java.util.Scanner;
-
  public class SqRtAprox {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -15,18 +14,15 @@ import java.util.Scanner;
         System.out.println("The square root of " + num + " is approximately " + sqrt(num));
         scanner.close();
     }
-
-
     public static double sqrt(long n) {
         double lastGuess = 1.0;
         double nextGuess = (lastGuess + n / lastGuess) / 2.0;
-
         while (Math.abs(nextGuess - lastGuess) >= 0.0001) {
             lastGuess = nextGuess;
             nextGuess = (lastGuess + n / lastGuess) / 2.0;
         }
-
         return nextGuess;
     }
 }
-
+// Enter a number to find its square root: 5
+// The square root of 5 is approximately 2.236067977499978
